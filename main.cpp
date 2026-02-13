b4a45c3d7a500b3d22a0eb39a4124385b165a230
KonstantinZZ <kostyazamik@gmail.com>
2026-02-13 23:33:32 +0500

    refactor: improve code style

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@ using namespace std;
 #include "std_lib_facilities.h"
 
 
+constexpr int ALPHABET_SIZE = 26;
+constexpr char FIRST_LETTER = 'a';
+
 int main()
 {
-    int i = 0; // исправил 
-
-    while (i < 26){
-        cout << char('a' + i) << "\t" 
-            << ('a' + i) << "\n";
-        i++;
+    for (int i = 0; i < ALPHABET_SIZE; ++i) {
+        char letter = FIRST_LETTER + i;
+        int ascii_code = FIRST_LETTER + i;
+        std::cout << letter << "\t" << ascii_code << "\n"; 
     }
+    return 0;
 }
