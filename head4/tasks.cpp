6ab88cd3fe4f5dc49fa0d60dda3285a9a2eeb611
KonstantinZZ <kostyazamik@gmail.com>
2026-02-13 23:53:37 +0500

    ready to new task

--- head4/tasks.cpp
+++ head4/tasks.cpp
@@ -86,3 +86,23 @@
         cout << "Извините , ошибка!\n";
         break;
     }
+
+
+
+    #include<iostream>
+using namespace std;
+#include "std_lib_facilities.h"
+
+// улучшенный код перебора значений
+constexpr int ALPHABET_SIZE = 26;
+constexpr char FIRST_LETTER = 'a';
+
+int main()
+{
+    for (int i = 0; i < ALPHABET_SIZE; ++i) {
+        char letter = FIRST_LETTER + i;
+        int ascii_code = FIRST_LETTER + i;
+        std::cout << letter << "\t" << ascii_code << "\n"; 
+    }
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -3,15 +3,11 @@ using namespace std;
 #include "std_lib_facilities.h"
 
 
-constexpr int ALPHABET_SIZE = 26;
-constexpr char FIRST_LETTER = 'a';
+
 
 int main()
 {
-    for (int i = 0; i < ALPHABET_SIZE; ++i) {
-        char letter = FIRST_LETTER + i;
-        int ascii_code = FIRST_LETTER + i;
-        std::cout << letter << "\t" << ascii_code << "\n"; 
-    }
-    return 0;
+
+
+
 }
